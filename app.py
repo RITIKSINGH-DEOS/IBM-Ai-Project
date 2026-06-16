920fe95a2ad76bbe80aae0fc9bfedd13bcd39a76
RIITK SINGH <businessritiksinghdeos@gmail.com>
2026-06-16 10:08:12 +0530

    Updated Code

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ import os
 
 load_dotenv()
 
+api_key = os.getenv("GEMINI_API_KEY")
 
+if not api_key:
+    api_key = st.secrets["GEMINI_API_KEY"]
 
-genai.configure(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
+genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
